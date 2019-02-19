4d6e01550596638840afe367cc624980ff62e3f6
EwelinaGruszczynska <46709829+EwelinaGruszczynska@users.noreply.github.com>
2019-02-19 15:11:03 +0100

    zajęcia dn.19.02.2019

--- book-library/src/main/java/com/blackbeast/booklibrary/repository/BookRepository.java
+++ book-library/src/main/java/com/blackbeast/booklibrary/repository/BookRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.util.Collection;
 
@@ -40,8 +41,46 @@ public class BookRepository {
             em.remove(book);
     }
     public Collection<Book> getBooksByAuthor(String authorName){
-        return em.createQuery("SELECT DISTINCT b from Book b INNER JOIN b.author a WHERE a.name =:authorName", Book.class)
-                .setParameter("authorName", authorName)
+        return em.createQuery("from Book b WHERE LOWER (b.author.name) LIKE CONCAT('%',:authorName, '%')", Book.class)
+                .setParameter("authorName", authorName.toLowerCase())
                 .getResultList();
     }
+
+    public Collection<Book> getBooks(Integer year, String publisher, String isbn) {
+        String query = "from Book b";
+        String conditions = "";
+
+        if (year != null)
+            conditions += "b.year = :year";
+
+        if (publisher != null)
+            if (conditions.isEmpty())
+                conditions += "b.publisher = :publisher";
+            else
+                conditions += " AND b.publisher = :publisher";
+
+        if (isbn != null)
+            if (conditions.isEmpty())
+                conditions += "b.isbn = :isbn";
+            else
+                conditions += " AND b.isbn = :isbn";
+
+        if (!conditions.isEmpty())
+            query += " WHERE " + conditions;
+
+        System.out.println(">>>>>>>>>>>>>>>>>>>>" + query);
+
+        TypedQuery typedQuery = em.createQuery(query, Book.class);
+
+        if(year != null)
+            typedQuery.setParameter("year", year);
+
+        if(publisher != null)
+            typedQuery.setParameter("publisher", publisher);
+
+        if(isbn != null)
+            typedQuery.setParameter("isbn", isbn);
+
+        return typedQuery.getResultList();
+        }
 }
--- book-library/src/main/java/com/blackbeast/booklibrary/restcontrollers/BookRestController.java
+++ book-library/src/main/java/com/blackbeast/booklibrary/restcontrollers/BookRestController.java
@@ -5,6 +5,7 @@ import com.blackbeast.booklibrary.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -68,6 +69,36 @@ public class BookRestController {
         if (book == null)
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(book, HttpStatus.OK);
+
+    }
+
+    @RequestMapping(value = "/books/getByAuthor", method = RequestMethod.GET)
+    public ResponseEntity<List<Book>> getBooksByAuthor (@RequestParam(name = "author", required = true) String authorName){
+     List<Book> books = bookService.getBooksByAuthor(authorName);
+
+     if(books == null)
+         return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
+     return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/books/get", method = RequestMethod.GET)
+    public ResponseEntity<List<Book>> getBooks(@RequestParam(value = "year", required = false) Integer year,
+                                               @RequestParam(value = "publisher", required = false) String publisher,
+                                               @RequestParam(value = "isbn", required = false) String isbn){
+        List<Book> books = bookService.getBooks(year, publisher, isbn);
+
+        if(books == null)
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public  ResponseEntity<ExceptionDetail> exceptionHandler(MissingServletRequestParameterException ex) {
+        ExceptionDetail exceptionDetail = new ExceptionDetail(ex.getClass().getSimpleName(), ex.getMessage());
+
+        return new ResponseEntity<>(exceptionDetail, HttpStatus.BAD_REQUEST);
 
     }
 }
