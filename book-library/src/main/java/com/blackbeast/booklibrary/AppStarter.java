ae850090cee5e543b012ba5cb82c5866d92ff1f1
EwelinaGruszczynska <46709829+EwelinaGruszczynska@users.noreply.github.com>
2019-03-04 14:56:52 +0100

    zajęcia 04.03.2019

--- book-library/src/main/java/com/blackbeast/booklibrary/AppStarter.java
+++ book-library/src/main/java/com/blackbeast/booklibrary/AppStarter.java
@@ -2,7 +2,9 @@ package com.blackbeast.booklibrary;
 
 import com.blackbeast.booklibrary.domain.Author;
 import com.blackbeast.booklibrary.domain.Book;
+import com.blackbeast.booklibrary.domain.User;
 import com.blackbeast.booklibrary.services.BookService;
+import com.blackbeast.booklibrary.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.PropertySource;
@@ -14,10 +16,13 @@ public class AppStarter implements CommandLineRunner {
 
     @Autowired
     BookService bookService;
+    @Autowired
+    UserService userService;
 
     @Override
     public void run(String... args) throws Exception {
         init();
+        initUser();
     }
 
     public void init(){
@@ -29,5 +34,16 @@ public class AppStarter implements CommandLineRunner {
 
         Book book3 = new Book("Pan Wołodyjowski", 1999, "PWN", "54671724546", new Author("Henryk Sieniewicz"));
         bookService.saveBook(book3);
+    }
+
+    public void initUser(){
+        userService.createUser("admin", "pass");
+        userService.addRoleToUser("admin", "ADMIN");
+        userService.addRoleToUser("admin", "DEV");
+        userService.addRoleToUser("admin", "USER");
+
+        userService.createUser("user", "pass");
+        userService.addRoleToUser("user", "USER");
+
     }
 }
