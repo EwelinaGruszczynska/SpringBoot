1fb1d3c640463288c409317ea8c111e32b212dc6
EwelinaGruszczynska <46709829+EwelinaGruszczynska@users.noreply.github.com>
2019-03-05 14:57:25 +0100

    zajecia 05.03

--- book-library/src/main/java/com/blackbeast/booklibrary/Security/SecurityConfig.java
+++ book-library/src/main/java/com/blackbeast/booklibrary/Security/SecurityConfig.java
@@ -1,17 +1,23 @@
 package com.blackbeast.booklibrary.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -21,9 +27,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-                .antMatchers("/h2-console").permitAll();
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/").hasAnyAuthority("ADMIN", "USER")
+                .antMatchers("/books").hasAnyAuthority("ADMIN", "USER")
+                .antMatchers("/books/api").hasAnyAuthority("ADMIN", "USER")
+                .anyRequest().authenticated().and().formLogin().defaultSuccessUrl("/books");
+
+        httpSecurity.authorizeRequests()
+                .antMatchers("/").hasAnyAuthority("ADMIN", "USER");
 
         httpSecurity.headers().frameOptions().disable();
         httpSecurity.csrf().ignoringAntMatchers("/h2-console/**");
 }
+    @Autowired
+    public  void  securityUsers(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("SELECT USERNAME, PASSWORD, ENABLED FROM USER u WHERE u.USERNAME = ?")
+                .authoritiesByUsernameQuery("SELECT USERNAME, NAME FROM USER u JOIN ROLE r ON u.ID = r.USER_ID WHERE u.USERNAME =?");
+
+    }
 }
--- book-library/src/main/java/com/blackbeast/booklibrary/services/UserService.java
+++ book-library/src/main/java/com/blackbeast/booklibrary/services/UserService.java
@@ -4,6 +4,8 @@ import com.blackbeast.booklibrary.domain.Role;
 import com.blackbeast.booklibrary.domain.User;
 import com.blackbeast.booklibrary.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -16,16 +18,15 @@ public class UserService {
 
     public void createUser(String username, String password) {
         if (username != null && password != null) {
-            User user = userRepository.getUser(username);
 
-            if (user == null){
-                User newUser = new User (username, password);
-                userRepository.addUser(newUser);
+            PasswordEncoder pe = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+            User user = new User (username, pe.encode(password));
+            userRepository.addUser(user);
 
         }
     }
 
-}
+
 
     public void addRoleToUser (String username, String roleName){
         if (username!= null && roleName != null){
